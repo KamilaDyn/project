f4bea7f3097176774b7afe0edaef12ba2013d75a
Kamila Dynysiuk <kamila0990@gmail.com>
2019-04-05 21:24:04 +0100

    new files

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const quotes = ['"Two things are infinite the universe and human stupidity; and I\'m not sure about the universe. "', '"A room without books is like a body without a soul.” ', '“We are all in the gutter, but some of us are looking at the stars.”', '“If you judge people, you have no time to love them.” ', '“I love deadlines. I love the whooshing noise they make as they go by.” '];
+
+const button = document.querySelector('button');
+const p = document.querySelector('p');
+const randomQuotes = () => {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    // return quotes[randomIndex];
+    p.textContent = `${quotes[randomIndex]}`;
+};
+button.addEventListener('click', randomQuotes);
