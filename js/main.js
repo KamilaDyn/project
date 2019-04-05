5b118cbca449ef42bcb8ac0ea945ade5cd85189e
Kamila Dynysiuk <kamila0990@gmail.com>
2019-04-05 22:37:59 +0100

    add quote

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const quotes = ['"Two things are infinite the universe and human stupidity; and I\'m not sure about the universe. "', '"A room without books is like a body without a soul.” ', '“We are all in the gutter, but some of us are looking at the stars.”', '“If you judge people, you have no time to love them.” ', '“I love deadlines. I love the whooshing noise they make as they go by.” '];
+const quotes = ['"Two things are infinite the universe and human stupidity; and I\'m not sure about the universe. "', '"A room without books is like a body without a soul.” ', '“We are all in the gutter, but some of us are looking at the stars.”', '“If you judge people, you have no time to love them.” ', '“I love deadlines. I love the whooshing noise they make as they go by.”, "Be yourself; everyone else is already taken." '];
 
 const button = document.querySelector('button');
 const p = document.querySelector('p');
---
+++ readme.md
@@ -0,0 +1,1 @@
+project -exercise with git 
